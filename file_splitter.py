2f0615f0273082441e3bfafa93d620353aa39f24
MAJ0RRR <sebastian.lesniewski.edu@gmail.com>
2023-11-28 16:06:07 +0100

    Adjust splitting parameters

--- file_splitter.py
+++ file_splitter.py
@@ -9,7 +9,7 @@ class FileSpliter:
 		self.source = source
 		self.destination = destination
 		
-	def split_silence(self, silence_threshs: List[int], min_silence_lens: List[int], min_audio_len: int = 2500, max_audio_len: int = 15000):
+	def split_silence(self, silence_threshs: List[int], min_silence_lens: List[int], min_audio_len: int = 5, max_audio_len: int = 15000):
 		
 		for audiofile in os.listdir(self.source):
 			stem = pathlib.Path(audiofile).stem
@@ -21,7 +21,7 @@ class FileSpliter:
 			for silence_thresh, min_silence_len in zip(silence_threshs, min_silence_lens):
 				chunks_new_iter = []
 				for chunk in chunks:
-					new_chunks = pydub.silence.split_on_silence(chunk, min_silence_len=min_silence_len, silence_thresh=silence_thresh, keep_silence=50)
+					new_chunks = pydub.silence.split_on_silence(chunk, min_silence_len=min_silence_len, silence_thresh=silence_thresh, keep_silence=100)
 					for new_chunk in new_chunks:
 						if len(new_chunk) < min_audio_len:
 							pass
